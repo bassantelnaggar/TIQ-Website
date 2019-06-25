8698f4dda5e7d234ce1338415f65c9dacab2570a
Nadin-ElHanafy <nandms17@gmail.com>
2019-06-25 12:59:11 +0200

    personal profiles

--- client/src/components/users/profile.js
+++ client/src/components/users/profile.js
@@ -3,11 +3,11 @@ import {connect} from 'react-redux';
 import axios from "axios"
 import "./profile.css"
 import Toolbar from "../../layout/Toolbar/Toolbar"
-import PegasusLogo from "../../components/images/tiqLogo1.png"
-import OrionLogo from "../../Images/rsz_1rsz_orion-logo.jpg"
-import PegasusLogo3 from "../../Images/PEGASUS-LOGO.jpeg"
-import {Container} from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import Background from "../../Images/about.jpg"
+
+
+
+
 const mapStateToProps = state => {
     return { token: state.token, usertype: state.usertype, id: state.id };
   };
@@ -22,6 +22,36 @@ class profile extends Component {
       //auth : true
     }
   }
+
+  onChange = (e) => {
+    var file = e.target.files[0];
+    console.log(e.target.files[0]);
+
+
+
+    var formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset','zcwrt7qz');
+
+    axios.post(
+        'https://api.cloudinary.com/v1_1/dpny1nhaq/image/upload',
+        formData,
+        // method: 'POST',
+        {headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }}
+    ).then((res)=>{
+        console.log(res);
+        axios.put(`http://localhost:5000/api/Users/Profile/${this.props.id}`,{
+            'profilePicture': res.data.secure_url
+        })
+        window.location.reload()
+    }).catch(function(err){
+        console.log(err);
+    });
+
+}
+
 
   componentDidMount()
   {
@@ -39,8 +69,9 @@ class profile extends Component {
   {
     if (this.props.token == null) {
       return (
-        <div>
+        <div >
           <Toolbar />
+         
           <div class="thumbnails">
             <div class="box">
               <div class="inner">
@@ -58,56 +89,75 @@ class profile extends Component {
             </div>
           </div>
         </div>
+        
       );
     }
     else{
     return(
       <div>
-        <Toolbar/>
-      <figure class="snip0057 red hover">
-  <figcaption>
-    <h2>{this.state.user.firstName} <span>{this.state.user.lastName}</span></h2>
-    <p> {this.state.user.bio}</p>
-    <p>{this.state.user.house}</p> 
-    <p>{this.state.user.birthDate}</p>  
-    <br></br>
-    <br></br>
-    <br></br>
-    {/* <div class="icons"><a href="#"><i class="ion-ios-home"></i></a><a href="#"><i class="ion-ios-email"></i></a><a href="#"><i class="ion-ios-telephone"></i></a></div> */}
-  </figcaption>
-   <div class="image"><img src={PegasusLogo } alt="sample4"/></div> 
-  <div class="position">{this.state.user.type}</div>
-</figure>
-{/* <figure class="snip0057 blue">
-  <figcaption>
-    <h2>Tiffany <span>Case</span></h2>
-    <p>That's the whole problem with science. You've got a bunch of empiricists trying to describe things of unimaginable wonder.</p>
-    <div class="icons"><a href="#"><i class="ion-ios-home"></i></a><a href="#"><i class="ion-ios-email"></i></a><a href="#"><i class="ion-ios-telephone"></i></a></div>
-  </figcaption>
-  <div class="image"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sq-sample3.jpg" alt="sample3"/></div>
-  <div class="position">Software Engineer</div>
-</figure> */}
+      <img src={Background} style={{backgroundsize: 'cover', marginTop:"-5%"}} />
+     <div >
+       <Toolbar/> 
+      
+       
+          `<section class="card">
+              <figure class="panel meta">
+              <picture>
+              <div class="image-upload">
+              <label for="file-input">
+              <img class="avatar" src={this.state.user.profilePicture} width="128" height="128"/> 
+              </label> 
+              <input id="file-input" accept=".jpg,.png" type="file" onChange={this.onChange} display="none"/>
+              </div>
+              </picture>
+              <figcaption>
+
+              <h1 class="name">{this.state.user.firstName} {this.state.user.lastName} </h1>
+              <h3 class="title">{this.state.user.bio}</h3>
+              </figcaption>
+              </figure>
+          
+              <div class="panel info">
+                <dl>
+                <dt>
+                  <h4 className="text">Birth Day </h4>
+                  <h5 className="text">{this.state.user.birthDate}</h5>
+                </dt>
+                {/* <dt></dt> */}
+                <dd>98</dd>
+                <dt>
+                  <h4 className="text">Type</h4>
+                  <h5 className="text">{this.state.user.type}</h5>
+                </dt>
+                {/* <dt></dt> */}
+                <dd>98</dd>
+                <dt>
+                  <h4 className="text">Score</h4>
+                  <h5 className="text">{this.state.user.score}</h5>
+                </dt>
+                {/* <dt></dt> */}
+                <dd>98</dd>
+                <dt>
+                  <h4 className="text">House</h4>
+                  <h5 className="text">{this.state.user.house}</h5>
+                </dt>
+                <dd>98</dd>
+                <dt>
+                  <h4 className="text">TIQ Status</h4>
+                  <h5 className="text">{this.state.user.tiqStatus}</h5>
+                </dt>
+                <dd>98</dd>
+                </dl>
+            
+            
+            
+          </div>
+      
+    </section>`
+
+
 </div>
-  // <div className="profilePage7">
-  //     <Toolbar/>
-  //     {/* <Container className="nadin"> */}
-  //     <ul className="profile7">
-  //     <image src={this.state.user.profilePicture} className="profilepic7"></image>
-  //     <br/>
-  //    <p className="Name">Name: {this.state.user.firstName} {this.state.user.lastName}</p>
-  //    <br></br>
-  //    <p className="Email">Email: {this.state.user.email}</p>
-  //    <br></br>
-  //    <p className="BD">birthday: {this.state.user.birthDate}</p>
-  //    <br></br>
-  //    <p className="C">clubs:{this.state.user.clubs}</p>
-  //    <br/>
-  //    <p className="B">Bio: {this.state.user.bio}</p>
-  //    <br/>
-  //    <p className="T">Type: {this.state.user.type}</p> 
-  //   </ul>   
-  //   {/* </Container> */}
-  // </div>
+</div>
   )
   }}
 }
